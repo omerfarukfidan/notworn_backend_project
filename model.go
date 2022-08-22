7f8360070f32c096afadd7d27d48d97e143b3319
omerfarukfidan <fidanomerfaruk01@gmail.com>
2022-08-22 21:10:53 +0300

    Huge Update

--- model.go
+++ model.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"gorm.io/gorm"
+	"mime/multipart"
 	"time"
 )
 
 type NotWorn struct {
-	Title       string         `json:"title"`
-	ID          uint           `gorm:"primaryKey" json:"id"`
-	Description string         `json:"description"`
-	Condition   string         `json:"condition"`
-	Price       uint           `json:"price"`
-	CompanyName string         `json:"companyName"`
-	TaskForce   string         `json:"taskForce"`
-	Location    string         `json:"location"`
-	Email       string         `json:"email"`
-	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at"`
-	CreateAt    time.Time      `json:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_At"`
+	Title       string         `form:"title"`
+	ID          uint           `gorm:"primaryKey" form:"id"`
+	Description string         `form:"description"`
+	Condition   string         `form:"condition"`
+	Price       uint           `form:"price"`
+	CompanyName string         `form:"companyName"`
+	TaskForce   string         `form:"taskForce"`
+	Location    string         `form:"location"`
+	Email       string         `form:"email"`
+	FileName    string         `form:"filename"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" form:"deleted_at"`
+	CreateAt    time.Time      `form:"created_at"`
+	UpdatedAt   time.Time      `form:"updated_At"`
+}
+type file struct {
+	ID     uint                  `form:"id"`
+	Avatar *multipart.FileHeader `form:"avatar" binding:"required"`
 }
--- router.go
+++ router.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 	"net/http"
+	"strconv"
 )
 
 type Server struct {
@@ -24,13 +26,13 @@ func NewServer(db *gorm.DB) *Server {
 
 func (s *Server) InitRoutes() {
 	s.Router.POST("", s.CreateNotWornHandler)
-
+	s.Router.PUT("/notworn/:id", s.AddImageHandler)
 }
 
 func (s *Server) CreateNotWornHandler(c *gin.Context) {
 
 	var notworn NotWorn
-	if err := c.BindJSON(&notworn); err != nil {
+	if err := c.ShouldBind(&notworn); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"status": "error", "error": err.Error()})
 
 		return
@@ -45,5 +47,41 @@ func (s *Server) CreateNotWornHandler(c *gin.Context) {
 	}
 
 	c.IndentedJSON(http.StatusCreated, notworn)
+
+}
+
+func (s *Server) AddImageHandler(c *gin.Context) {
+
+	var userObj file
+	if err := c.ShouldBind(&userObj); err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	id := c.Params.ByName("id")
+	intVar, err := strconv.Atoi(id)
+	obj, err := GetNotWorn(s.DB, intVar)
+	fmt.Println(obj)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "unknown error")
+		return
+	}
+
+	WriteFileName(s.DB, &obj, &userObj)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "unknown error")
+		return
+	}
+
+	err = c.SaveUploadedFile(userObj.Avatar, "assets/"+userObj.Avatar.Filename)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "unknown error")
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+		"data":   userObj,
+	})
 
 }
